60ab6406efa3f68346f30278a546d4ac59f94c28
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-05 19:01:24 +0330

    add: prescript

--- dataset/main.py
+++ dataset/main.py
@@ -1,3 +1,20 @@
+# const variables
+VULNERABILITIES_PATH = "./docs/vulnerabilities.txt"
+ATTACKS_PATH = "./docs/attacks.txt"
+
+
 # base lists
 vulnerabilities = []
 attacks = []
+
+
+# read input documents
+with open(VULNERABILITIES_PATH, 'r', encoding='UTF-8') as file:
+    while line := file.readline():
+        vulnerabilities.append(line.rstrip())
+
+with open(ATTACKS_PATH, 'r', encoding='UTF-8') as file:
+    while line := file.readline():
+        attacks.append(line.rstrip())
+
+print(f'[INFO] loaded {len(attacks)} attacks, {len(vulnerabilities)} vulnerabilities into system.')
